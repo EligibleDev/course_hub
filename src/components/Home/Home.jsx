cc1244ae88458337e44a4221b5f3ffa953340c68
EligibleDev <eligibledev@gmail.com>
2023-09-15 06:36:13 +0600

    card design done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,45 @@
+import './Home.css'
+import { FiDollarSign } from 'react-icons/fi';
+import { BsBook } from 'react-icons/bs';
+import { useEffect, useState } from 'react';
+
 const Home = () => {
+    const [allCourses, setAllCourses] = useState([])
+
+    useEffect(() => {
+        fetch('./data.json')
+            .then(res => res.json())
+            .then(data => setAllCourses(data));
+    }, [])
+
+    console.log(allCourses);
+
     return (
-        <div>
-            
+        <div className='main'>
+            <div className="card-container">
+                {allCourses.map(course =>
+                    <div key={course.id} className="card">
+                        <img src={course.img} alt="" />
+                        <h2 className='title'>{course.title}</h2>
+
+                        <p className='description'>{course.description}</p>
+
+                        <div className='info'>
+                            <div>
+                                {<FiDollarSign></FiDollarSign>}
+                                <p className='info-item'>Price: ${course.price}</p>
+                            </div>
+
+                            <div>
+                                {<BsBook></BsBook>}
+                                <p className='info-item'>Credit: {course.duration_hours}hr</p>
+                            </div>
+                        </div>
+
+                        <button className='select'>Select</button>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
